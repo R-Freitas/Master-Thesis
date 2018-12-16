f48fb708e1ec3eb44131234d1b3bf0742151c957
R-Freitas <34415835+R-Freitas@users.noreply.github.com>
2018-12-16 22:19:00 +0000

    Chanfes to CNN

--- CNN.py
+++ CNN.py
@@ -31,19 +31,19 @@ import cv2
 #Paths to save network weights and to obtain info
 image_directory = '../Treated_Data/Images'
 #weights_load_path = '../Results/Current_Training/best-04-0.84.hdf5'
-weights_load_path ='../Results/Current_Training/best-02-0.83.hdf5'
+weights_load_path = None
 weights_save_path = '../Results/Current_Training'
 history_save_directory = '../Results/History_Objects'
-history_name = '/1_treino_2C_withS'
+history_name = '/VGG_1_treino_2C_S_in_G2'
 
 #Image generator settings
-train_batch_size = 10
+train_batch_size = 100
 train_images_generated = 50000
 validate_batch_size = 10
 
 #Essential Network settings
 epochs = 10
-learning_rate = 0.001
+learning_rate = 0.01
 
 
 
@@ -77,39 +77,39 @@ def create_model(weights_path=None):
 def VGG_16(weights_path=None):
     model = Sequential()
     model.add(ZeroPadding2D((1,1),input_shape=(180, 180,1)))
-    model.add(Conv2D(64, 3, 3, activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(64, 3, 3, activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(128, 3, 3, activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(128, 3, 3, activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, 3, 3, activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, 3, 3, activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, 3, 3, activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(Flatten())
@@ -117,18 +117,19 @@ def VGG_16(weights_path=None):
     model.add(Dropout(0.5))
     model.add(Dense(4096, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(3, activation='softmax'))
+    model.add(Dense(1, activation=tf.nn.sigmoid))
 
     if weights_path:
         model.load_weights(weights_path)
 
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(optimizer=Adam(),
-                  loss='categorical_crossentropy',
+    model.compile(optimizer=Adam(lr= learning_rate),
+                  loss='binary_crossentropy',
                   metrics=['accuracy'])
 
 
     return model
+
 def test_channel_pos():
     if K.image_data_format() == 'channels_first':
         print("FIRST")
@@ -138,7 +139,8 @@ def test_channel_pos():
         exit()
 
 print("RUNNING CONVULOTIONAL NETWORK")
-CNN_Network = create_model(weights_load_path)
+#CNN_Network = create_model(weights_load_path)
+CNN_Network = VGG_16(weights_load_path)
 
 train_datagen = ImageDataGenerator(
                 rescale = 1./255.,
@@ -150,6 +152,7 @@ train_datagen = ImageDataGenerator(
                 fill_mode = 'constant',
                 cval = 0,
                 data_format='channels_last')
+
 test_datagen = ImageDataGenerator(
                rescale = 1./255.,
                data_format='channels_last')
@@ -161,6 +164,7 @@ train_generator = train_datagen.flow_from_directory(
         color_mode = 'grayscale',
         class_mode = 'binary',
         shuffle = 'True')
+
 validation_generator = test_datagen.flow_from_directory(
         image_directory + '/test',  # this is the target directory
         target_size = (180, 180),
@@ -169,10 +173,17 @@ validation_generator = test_datagen.flow_from_directory(
         class_mode = 'binary',
         shuffle = 'True')
 
+cross_validation_generator = test_datagen.flow_from_directory(
+        image_directory + '/validate',  # this is the target directory
+        target_size = (180, 180),
+        batch_size = validate_batch_size,
+        color_mode = 'grayscale',
+        class_mode = 'binary',
+        shuffle = 'True')
 
 
 
-TESTING_MODE=1
+TESTING_MODE=0
 if TESTING_MODE == 0 :
 
 
@@ -211,4 +222,4 @@ if TESTING_MODE == 0 :
     plt.show()
 
 else:
-    print(CNN_Network.evaluate_generator(validation_generator, steps=len(validation_generator), max_queue_size=10, workers=1, use_multiprocessing=False))
+    print(CNN_Network.evaluate_generator(cross_validation_generator, steps=len(cross_validation_generator), max_queue_size=10, workers=1, use_multiprocessing=False))
